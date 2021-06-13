81ca477b0c00d9d11794fb260da3e9b6e755fda5
daniel <danielderu@gmail.com>
2021-06-13 20:16:29 +0200

    fixed the leaflet bug and got the map to show with the current location of the user

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import path from "path"
+
+const server = express()
+const __dirname = path.resolve()
+
+server.use(express.static(path.join("../", "client", "build")))
+
+server.get("/*", (req, res) => {
+    res.sendFile(path.join("../", "client", "build", "index.html"))
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
