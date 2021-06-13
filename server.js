0e668d5e5595a2034b0372313b97f88dcf8c88bd
daniel <danielderu@gmail.com>
2021-06-13 22:19:11 +0200

    test run 1

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ import path from "path"
 const server = express()
 const __dirname = path.resolve()
 
-server.use(express.static(path.join("client", "build")))
+// server.use(express.static(path.join("client", "build")))
 
-server.get("/*", (req, res) => {
-    res.sendFile(path.join("client", "build", "index.html"))
+// server.get("/*", (req, res) => {
+//     res.sendFile(path.join("client", "build", "index.html"))
+// })
+
+server.get("/", (req, res) => {
+    res.send("this is working")
 })
 
 const PORT = process.env.PORT || 5000
